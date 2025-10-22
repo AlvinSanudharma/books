4a2a358daa58a54bb8f00d9e02e4f59399b43b3b
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-10-22 16:00:57 +0800

    refactor: controller class name

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index'])->name('index');
+Route::get('/home', [BookController::class, 'index'])->name('index');
